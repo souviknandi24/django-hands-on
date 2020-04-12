8bef4b81cff992caec6de3b0b89220d1020a785d
souviknandi24 <souviknandi24@gmail.com>
2020-04-12 05:47:51 +0530

    added associations and updated the directory structure

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -1,28 +1,4 @@
 # serializers.py
-from rest_framework import serializers
-
-from .models import Hero, Todo, Task
-
-
-class HeroSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Hero
-        fields = ['name', 'alias']
-
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Todo
-        fields = ['name', 'start_date', 'end_date']
-
-class TaskSerializer(serializers.HyperlinkedModelSerializer):
-    included_serializers = {
-        'todo': TodoSerializer
-    }
-
-    class Meta:
-        model = Task
-        fields = ['name', 'todo']
-    
-    class JSONAPIMeta:
-        select_related = ['todo']
-
+from .components.heroes import HeroSerializer
+from .components.todos import TodoSerializer
+from .components.tasks import TaskSerializer
